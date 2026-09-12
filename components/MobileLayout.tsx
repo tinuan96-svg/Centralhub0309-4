f5428ee26df029ce5698ce6513fd0e2710d46d58
tinuan96-svg <tinuan96@gmail.com>
2026-09-12 06:22:44 +0100

    feat: surface NORA computer mode launcher

--- components/MobileLayout.tsx
+++ components/MobileLayout.tsx
@@ -9,6 +9,7 @@ import DhlInvoiceAutoSync from './DhlInvoiceAutoSync';
 import CentralHubLiveUpdate from './CentralHubLiveUpdate';
 import CentralHubVoiceAssistant from './CentralHubVoiceAssistant';
 import NoraLiveActionOverlay from './NoraLiveActionOverlay';
+import NoraComputerLauncher from './NoraComputerLauncher';
 import QuickActionsFab from '@/app/dashboard/components/QuickActionsFab';
 import { useMediaQuery } from '@/lib/hooks/useMediaQuery';
 import { useState, useEffect } from 'react';
@@ -46,6 +47,7 @@ export default function MobileLayout({ children }: MobileLayoutProps) {
       </main>
       {pathname === '/dashboard' && <QuickActionsFab />}
       <CentralHubVoiceAssistant />
+      <NoraComputerLauncher />
       <NoraLiveActionOverlay />
       <MobileBottomNav />
     </div>
@@ -58,6 +60,7 @@ export default function MobileLayout({ children }: MobileLayoutProps) {
       <div className="flex-1 min-w-0 flex flex-col overflow-hidden"><Topbar /><main className="flex-1 min-h-0 min-w-0 overflow-y-auto overscroll-contain pb-safe-bottom pr-safe-right pl-safe-left"><div className="max-w-[1920px] min-w-0 mx-auto w-full">{children}</div></main></div>
       {pathname === '/dashboard' && <QuickActionsFab />}
       <CentralHubVoiceAssistant />
+      <NoraComputerLauncher />
       <NoraLiveActionOverlay />
     </div>
   );
