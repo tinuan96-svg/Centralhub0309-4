af5518bcab4e22c56d59c7927ecd3cd9476b52b8
tinuan96-svg <tinuan96@gmail.com>
2026-09-12 10:43:57 +0100

    feat: mount Shruthi visual identity across CentralHub

--- components/MobileLayout.tsx
+++ components/MobileLayout.tsx
@@ -12,6 +12,7 @@ import CentralHubVoiceAssistant from './CentralHubVoiceAssistant';
 import NoraConversationSessionSync from './NoraConversationSessionSync';
 import NoraLiveActionOverlay from './NoraLiveActionOverlay';
 import NoraComputerLauncher from './NoraComputerLauncher';
+import ShruthiIdentitySkin from './ShruthiIdentitySkin';
 import QuickActionsFab from '@/app/dashboard/components/QuickActionsFab';
 import { useMediaQuery } from '@/lib/hooks/useMediaQuery';
 import { useState, useEffect } from 'react';
@@ -48,6 +49,7 @@ export default function MobileLayout({ children }: MobileLayoutProps) {
         </div>
       </main>
       {pathname === '/dashboard' && <QuickActionsFab />}
+      <ShruthiIdentitySkin />
       <NoraAdaptiveVoiceNormalizer />
       <CentralHubVoiceAssistant />
       <NoraConversationSessionSync />
@@ -63,6 +65,7 @@ export default function MobileLayout({ children }: MobileLayoutProps) {
       <ClassifiedSidebar />
       <div className="flex-1 min-w-0 flex flex-col overflow-hidden"><Topbar /><main className="flex-1 min-h-0 min-w-0 overflow-y-auto overscroll-contain pb-safe-bottom pr-safe-right pl-safe-left"><div className="max-w-[1920px] min-w-0 mx-auto w-full">{children}</div></main></div>
       {pathname === '/dashboard' && <QuickActionsFab />}
+      <ShruthiIdentitySkin />
       <NoraAdaptiveVoiceNormalizer />
       <CentralHubVoiceAssistant />
       <NoraConversationSessionSync />
