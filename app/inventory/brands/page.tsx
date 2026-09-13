c9bba1651920a11c74baca81fc78a92dd57026a8
tinuan96-svg <tinuan96@gmail.com>
2026-09-13 18:06:23 +0100

    feat(brands): open brand workspace on graphical sales intelligence

--- app/inventory/brands/page.tsx
+++ app/inventory/brands/page.tsx
@@ -1,11 +1,11 @@
 import ProductWorkspaceNav from '@/components/products/ProductWorkspaceNav';
-import BrandsClient from '@/app/settings/master-data/brands/BrandsClient';
+import BrandSalesSection from './BrandSalesSection';
 
-export default function Page({ params, searchParams }: { params: any; searchParams: any }) {
+export default function Page() {
   return (
-    <div className="mx-auto max-w-[1800px] space-y-6 p-6">
+    <div className="mx-auto max-w-[1800px] space-y-6 p-4 fold-inner:p-5 lg:p-6">
       <ProductWorkspaceNav />
-      <BrandsClient params={params} searchParams={searchParams} />
+      <BrandSalesSection />
     </div>
   );
 }
