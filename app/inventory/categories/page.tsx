04ac7541c4955248ad26513e6cc258002d1b8109
tinuan96-svg <tinuan96@gmail.com>
2026-09-13 18:06:17 +0100

    feat(categories): open category workspace on graphical sales intelligence

--- app/inventory/categories/page.tsx
+++ app/inventory/categories/page.tsx
@@ -1,11 +1,11 @@
 import ProductWorkspaceNav from '@/components/products/ProductWorkspaceNav';
-import InventoryCategoriesClient from './InventoryCategoriesClient';
+import CategorySalesSection from './CategorySalesSection';
 
 export default function Page() {
   return (
     <div className="mx-auto max-w-[1800px] space-y-6 p-4 fold-inner:p-5 lg:p-6">
       <ProductWorkspaceNav />
-      <InventoryCategoriesClient />
+      <CategorySalesSection />
     </div>
   );
 }
